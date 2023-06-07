e040473a5b733b9a0f4aeaa76993d6056e90b064
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-07 21:01:29 +0600

    Login and registration page ui design

--- src/pages/Shared/Navbar/NavBar.jsx
+++ src/pages/Shared/Navbar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     const navOptions  = 
     <>
         <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'/instructor'}>Instructor</Link></li>
+        <li><Link to={'/instructor'}>Instructors</Link></li>
         <li><Link to={'/classes'}>Classes</Link></li>
     </>
 
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Instructors from "../pages/Instructors/Instructors";
+import Classes from "../pages/Classes/Classes";
+import Login from "../pages/SignIn/Login";
+import Register from "../pages/SignUp/Register";
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +15,24 @@ export const router = createBrowserRouter([
         {
             path: '/',
             element: <Home/>
-        }
+        },
+        {
+          path: '/instructor',
+          element: <Instructors/>
+        },
+        {
+          path: '/classes',
+          element: <Classes/>
+        },
+        {
+          path: '/login',
+          element: <Login/>
+        },
+        {
+          path: '/register',
+          element: <Register/>
+        },
+        
     ]
   },
 ]);
