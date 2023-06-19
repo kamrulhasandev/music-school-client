a1668ec44f0e08744317b191df7c1889b812d2d4
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-19 17:27:52 +0600

    Footer ui change

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../pages/Shared/Footer/Footer';
-import Navbar from '../pages/Shared/Navbar/NavBar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/NavBar";
 
 const Main = () => {
-    return (
-        <div className=''>
-            <Navbar/>
-            <Outlet/>
-            <div className='bg-slate-400 text-black'>
+  return (
+    <div className="">
+      <Navbar />
+      <Outlet />
 
-            <Footer/>
-            </div>
-        </div>
-    );
+      <div className="bg-[#f65209] text-white">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
